e55a291e7eb036189b33f2c71eb7e171aa30ec1d
I Made Raditya Bayu Pangestu <63552637+imrbp@users.noreply.github.com>
2024-03-13 19:54:00 +0800

    fix naming api from oder to orders

--- handler/order_handler.go
+++ handler/order_handler.go
@@ -11,11 +11,11 @@ func OrderHandler(controller controller.OrderController) http.Handler {
 	e := gin.New()
 
 	e.Use(gin.Recovery())
-	e.POST("/order", controller.Create)
-	e.PUT("/order/:orderId", controller.Update)
-	e.DELETE("/order/:orderId", controller.Delete)
-	e.GET("/order", controller.FindAll)
-	e.GET("/order/:orderId", controller.FindById)
+	e.POST("/orders", controller.Create)
+	e.PUT("/orders/:orderId", controller.Update)
+	e.DELETE("/orders/:orderId", controller.Delete)
+	e.GET("/orders", controller.FindAll)
+	e.GET("/orders/:orderId", controller.FindById)
 	e.Use(exception.ErrorHandler())
 	return e
 }
